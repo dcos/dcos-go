bcd11f0920722c2907e241408ab1221d3bfcb3b3
Maksym Naboka <maksym.naboka@gmail.com>
2016-09-27 13:40:05 -0700

    add run.sh helper

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IMG=$(docker images journal | grep journal | awk '{print $3}')
+
+if [ -z $IMG ] && [ -z $1 ]; then
+  echo "Could not detect docker image, please set tag 'journal' or pass"
+  echo "image ID"
+  echo "$0 <image-id>"
+  exit 1
+fi
+
+[ -z $IMG ] && IMG=$1
+
+[ -z $GOPATH ] && {
+  echo "\$GOPATH cannot be empty"
+  exit 1
+}
+
+container=$(docker run -d --privileged -v $GOPATH:/go -p8080:8080 $IMG /sbin/init)
+echo "# Using port :8080"
+echo "# Run:"
+echo "docker exec -it $container /bin/bash"
