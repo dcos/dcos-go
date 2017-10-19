797f0f2e70290321fce31e587e73c2383ebbbadf
Maksym Naboka <maksym.naboka@gmail.com>
2017-10-19 14:50:04 -0700

    add tests

--- dcos/nodeutil/util_test.go
+++ dcos/nodeutil/util_test.go
@@ -7,7 +7,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"context"
 	"github.com/dcos/dcos-go/dcos"
+	"io/ioutil"
+	"strings"
 )
 
 func TestDetectIP(t *testing.T) {
@@ -156,6 +159,95 @@ func TestClusterIDInvalidRole(t *testing.T) {
 			t.Fatalf("Expect error of type ErrNodeInfo. Got %s", err)
 		}
 	}
+}
+
+func TestMesosRuntimeShortCanonicalID(t *testing.T) {
+	expectedAgentID := "db10f9b1-5b82-4187-aa47-4fbcefc7cdca-S1"
+	expectedFrameworkID := "db10f9b1-5b82-4187-aa47-4fbcefc7cdca-0000"
+	expectedExecutorID := "single-mesos-container.c1f5ae3f-b81f-11e7-a9ac-52ad791ffaa8"
+	expectedContainerID := "1a69d257-48ca-4d3b-aead-332ad881fcc7"
+
+	if err := testCanonicalID("single-mesos-container", expectedAgentID, expectedFrameworkID, expectedExecutorID, expectedContainerID); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMesosRuntimeLongCanonicalID(t *testing.T) {
+	expectedAgentID := "db10f9b1-5b82-4187-aa47-4fbcefc7cdca-S1"
+	expectedFrameworkID := "db10f9b1-5b82-4187-aa47-4fbcefc7cdca-0000"
+	expectedExecutorID := "single-mesos-container.c1f5ae3f-b81f-11e7-a9ac-52ad791ffaa8"
+	expectedContainerID := "1a69d257-48ca-4d3b-aead-332ad881fcc7"
+	if err := testCanonicalID("single-mesos-container.c1f5ae3f-b81f-11e7-a9ac-52ad791ffaa8", expectedAgentID, expectedFrameworkID, expectedExecutorID, expectedContainerID); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCanonicalTaskIDNotFound(t *testing.T) {
+	if err := testCanonicalID("foobar", "", "", "", ""); err != ErrTaskNotFound {
+		t.Fatalf("error must be %s. Got %s", ErrTaskNotFound, err)
+	}
+}
+
+func TestPodCanonicalID(t *testing.T) {
+	expectedAgentID := "db10f9b1-5b82-4187-aa47-4fbcefc7cdca-S1"
+	expectedFrameworkID := "db10f9b1-5b82-4187-aa47-4fbcefc7cdca-0000"
+	expectedExecutorID := "instance-parent-pod.da6ef080-b81f-11e7-a9ac-52ad791ffaa8"
+	expectedContainerID := "e7ed292a-8390-4da4-8c2a-c13b554e2c2a-1eb53d03-e8f2-4de7-8a51-be17b42a3a29"
+	if err := testCanonicalID("container-1", expectedAgentID, expectedFrameworkID, expectedExecutorID, expectedContainerID); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCanonicalIDSameNameTasks(t *testing.T) {
+	err := testCanonicalID("test123", "", "", "", "")
+	if err == nil {
+		t.Fatal("expecting error. Got nil")
+	}
+
+	expectedError := "found more then 1 task with name test123: [test123-123 test123-345]"
+	if err.Error() != expectedError {
+		t.Fatalf("expect error %s. Got %s", expectedError, err)
+	}
+}
+
+func testCanonicalID(task, expectedAgentID, expectedFrameworkID, expectedExecutorID, expectedContainerID string) error {
+	state, err := ioutil.ReadFile("fixture/state.json")
+	if err != nil {
+		return err
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, string(state))
+	}))
+	defer ts.Close()
+
+	d, err := NewNodeInfo(&http.Client{}, dcos.RoleMaster, OptionMesosStateURL(ts.URL))
+	if err != nil {
+		return err
+	}
+
+	cID, err := d.TaskCanonicalID(context.TODO(), task)
+	if err != nil {
+		return err
+	}
+
+	if cID.AgentID != expectedAgentID {
+		return fmt.Errorf("expecting agent id %s. Got %s", expectedAgentID, cID.AgentID)
+	}
+
+	if cID.FrameworkID != expectedFrameworkID {
+		return fmt.Errorf("expecting framework id %s. Got %s", expectedFrameworkID, cID.FrameworkID)
+	}
+
+	if cID.ExecutorID != expectedExecutorID {
+		return fmt.Errorf("expecting executor id %s. Got %s", expectedExecutorID, cID.ExecutorID)
+	}
+
+	if strings.Join(cID.ContainerIDs, "-") != expectedContainerID {
+		return fmt.Errorf("expecting task id: %s. Got %s", expectedContainerID, cID.ContainerIDs)
+	}
+
+	return nil
 }
 
 func TestContextWithHeaders(t *testing.T) {
