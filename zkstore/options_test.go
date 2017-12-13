63709c94f0394b95a92f8c44426827992a8c0540
Collin Van Dyck <collinvandyck@gmail.com>
2017-12-13 10:34:40 -0500

    Add tests for the functional options.

---
+++ zkstore/options_test.go
@@ -0,0 +1,43 @@
+package zkstore
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"hash/fnv"
+	"testing"
+
+	"github.com/samuel/go-zookeeper/zk"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOptBasePath(t *testing.T) {
+	require := require.New(t)
+	store := &Store{}
+	require.EqualError(OptBasePath("foo")(store), "basePath must start with /")
+	require.NoError(OptBasePath("/foo")(store))
+}
+
+func TestOptHashBuckets(t *testing.T) {
+	require := require.New(t)
+	store := &Store{}
+	require.EqualError(OptNumHashBuckets(0)(store), "numBuckets must be positive")
+	require.EqualError(OptNumHashBuckets(-1)(store), "numBuckets must be positive")
+	require.NoError(OptNumHashBuckets(1)(store))
+}
+
+func TestOptACL(t *testing.T) {
+	require := require.New(t)
+	store := &Store{}
+	require.EqualError(OptACL(nil)(store), "ACL required")
+	require.EqualError(OptACL([]zk.ACL{})(store), "ACL required")
+	require.NoError(OptACL(zk.WorldACL(zk.PermAll))(store))
+}
+
+func TestOptHashProviderFunc(t *testing.T) {
+	require := require.New(t)
+	store := &Store{}
+	require.EqualError(OptHashProviderFunc(nil)(store), "hash provider func required")
+	require.NoError(OptHashProviderFunc(md5.New)(store))
+	require.NoError(OptHashProviderFunc(fnv.New128)(store))
+	require.NoError(OptHashProviderFunc(sha1.New)(store))
+}
