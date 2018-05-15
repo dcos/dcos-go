26a86f6f23d0e2f0d80bab2096338c19f2625ddb
Tim Weidner <tweidner@mesosphere.com>
2018-05-15 18:09:00 +0200

    Add missing DC/OS internal constants

--- dcos/constants.go
+++ dcos/constants.go
@@ -25,18 +25,3 @@ func GetFileDetectIPLocation() string {
 		return "/opt/mesosphere/bin/detect_ip"
 	}
 }
-
-// DC/OS DNS records.
-const (
-	// DNSRecordLeader is a domain name used by a leading master in DC/OS cluster.
-	DNSRecordLeader = "leader.mesos"
-)
-
-// DC/OS ports.
-const (
-	// PortMesosMaster defines a TCP port for mesos master.
-	PortMesosMaster = 5050
-
-	// PortMesosAgent defines a TCP port for mesos agent / public agent.
-	PortMesosAgent = 5051
-)
---
+++ dcos/ports.go
@@ -0,0 +1,25 @@
+package dcos
+
+// DC/OS ports.
+const (
+	// PortExhibitor defines a TCP port for Exhibitor.
+	PortExhibitor = 8181
+
+	// PortHistoryService defines a TCP port for the DC/OS history service.
+	PortHistoryService = 15055
+
+	// PortMarathonHTTP defines a TCP port for Marathon.
+	PortMarathonHTTP = 8080
+
+	// PortMesosAgent defines a TCP port for mesos agent / public agent.
+	PortMesosAgent = 5051
+
+	// PortMesosDNS defines a TCP port for MesosDNS.
+	PortMesosDNS = 8123
+
+	// PortMesosMaster defines a TCP port for mesos master.
+	PortMesosMaster = 5050
+
+	// PortMetronomeHTTP defines a TCP port for Metronome.
+	PortMetronomeHTTP = 9090
+)
